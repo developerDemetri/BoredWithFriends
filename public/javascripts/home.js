1a5a60026724be5dab69d66c8b71410b6872b764
DeveloperDemetri <developerdemetri@gmail.com>
2016-08-25 00:36:06 -0700

    setting up option cards

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -16,6 +16,7 @@ function setLocation() {
       loc.long = position.coords.longitude;
       $('#geoLoader').addClass("hide");
       $('#geoError').addClass("hide");
+      $('#explore-vs-home').removeClass("hide");
     });
   }
   else {
@@ -24,12 +25,34 @@ function setLocation() {
   }
 }
 
-function loadFood() {
-  console.log('what up fam');
-  var food_url = getServer()+'/suggestions/food/'+loc.lat+'/'+loc.long;
-  $.get(food_url).done(function(data) {
+function findFood() {
+  $('#explore-options').addClass("hide");
+  $('#find-food').removeClass("hide");
+  var find_food_url = getServer()+'/suggestions/food/find/'+loc.lat+'/'+loc.long;
+  $.get(find_food_url).done(function(data) {
     console.log(data);
   });
+}
+
+function orderFood() {
+  $('#home-options').addClass("hide");
+  $('#order-food').removeClass("hide");
+  var order_food_url = getServer()+'/suggestions/food/order/'+loc.lat+'/'+loc.long;
+  $.get(order_food_url).done(function(data) {
+    console.log(data);
+  });
+}
+
+function displayHomeOptions() {
+  $('#explore-vs-home').addClass("hide");
+  $('#explore-options').addClass("hide");
+  $('#home-options').removeClass("hide");
+}
+
+function displayExploreOptions() {
+  $('#explore-vs-home').addClass("hide");
+  $('#home-options').addClass("hide");
+  $('#explore-options').removeClass("hide");
 }
 
 function logout() {
